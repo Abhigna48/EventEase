22c2408c37e9092cea0a70dead1581a52b18d5d8
Abhigna48 <106460463+Abhigna48@users.noreply.github.com>
2023-09-08 08:13:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,23 @@
-const exp = require("express");//it imports a function createApplication
+const exp = require("express");
 const app=exp();
 app.listen(4803,()=>console.log("web server listening in port 4803!!"));
 //connect react field
 const path = require("path")
 app.use(exp.static(path.join(__dirname,'./build')))
-
-
-
 app.use(exp.json())
 //To craete db client and connect with respetive db and collectons
 const mclient = require("mongodb").MongoClient;
 mclient.connect("mongodb://127.0.0.1:27017")
 .then((dbRef)=>{
     const dbObj = dbRef.db('bdb');
-    const userCollection = dbObj.collection('users')
     const productCollection = dbObj.collection('products')
     const eventCollection = dbObj.collection('events')
     console.log('db connection success');
     //share collections to API
-    app.set('userCollection',userCollection)
     app.set('productCollection',productCollection)
     app.set('eventsCollection',eventCollection)
 })
 .catch(err=>console.log('error in db is ',err))
-
-
-
-
-
-//import userApp
-const userApp = require("./APIs/userApi");
-
-//execute use api when the request has user-api
-app.use('/user-api',userApp);
 
 const productApp = require("./APIs/productsApi");
 app.use('/product-api',productApp);
@@ -56,41 +41,3 @@ const ehmiddleware = (error,request,response,next)=>{
 };
 app.use(ehmiddleware);
 
-
-/*create middleware(if we make requests it wont be responded because it is a function and has to be called)
-//method1 executes for every req
-const middleware1 = (request,response,next)=>{
-    console.log("middle ware is called")
-    // forward middleware to next
-    next()
-    // can respond to client
-    //response.send({message:"unauthorised"})
-}
-//app.use(middleware)
-const middleware2 = (request,respond,next)=>{
-    console.log("2 executed")
-    next()//next should be called otherwise request stops here
-}
-app.use(middleware2)
-//create middleware for specific request by making it an argument in a request
-app.get('/get-product',middleware1,(request,response)=>{
-    response.send({message:"all products",payload:products});
-})
-//USER API
-//products
-
-
-//users
-let users=[]
-const middleware1 = (request,respond,next)=>{
-    console.log("middleware for every request is called")
-    next()
-}
-const middleware2 = (request,respond,next)=>{
-    console.log("middleware 2 executed")
-    next()
-}
-const middleware3 = (request,respond,next)=>{
-    console.log("middleware 3 executed")
-    next()
-}*/
